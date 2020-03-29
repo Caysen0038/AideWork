94221f88608d77b9560cbfa97933c93a362ee9bd
Caysen <617780725@qq.com>
2020-03-29 20:54:29 +0800

    MessageHelper增加SHA256算法

--- aidework-core/src/main/java/org/aidework/core/helper/MessageHelper.java
+++ aidework-core/src/main/java/org/aidework/core/helper/MessageHelper.java
@@ -58,6 +58,8 @@ public class MessageHelper {
         return encodeStr;
     }
 
+
+
     private static String byte2Hex(byte[] bytes) {
         StringBuffer stringBuffer = new StringBuffer();
         String temp = null;
@@ -70,48 +72,5 @@ public class MessageHelper {
             stringBuffer.append(temp);
         }
         return stringBuffer.toString();
-    }
-
-    public static String DeSHA256(String str) {
-
-        if (str == null) {
-
-            return null;
-        }
-
-        try {
-
-            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
-            messageDigest.reset();
-            messageDigest.update(str.getBytes());
-            return getFormattedText(messageDigest.digest());
-
-        } catch (Exception e) {
-            return "";
-        }
-
-    }
-
-    private static final char[] HEX_DIGITS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd',
-            'e', 'f' };
-
-    private static String getFormattedText(byte[] bytes) {
-
-        int len = bytes.length;
-
-        StringBuilder buf = new StringBuilder(len * 2);
-
-        // 把密文转换成十六进制的字符串形式
-
-        for (int j = 0; j < len; j++) {
-
-            buf.append(HEX_DIGITS[(bytes[j] >> 4) & 0x0f]);
-
-            buf.append(HEX_DIGITS[bytes[j] & 0x0f]);
-
-        }
-
-        return buf.toString();
-
     }
 }
